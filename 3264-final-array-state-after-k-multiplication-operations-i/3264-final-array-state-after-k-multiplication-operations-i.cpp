5ab0eaa743586b2882057b8e0a6aab49f5860155
Naveen300305 <133203837+Naveen300305@users.noreply.github.com>
2024-12-16 09:16:39 +0530

    Update 3264-final-array-state-after-k-multiplication-operations-i.cpp

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int m) {
-        vector<int> ans=nums;
+        priority_queue<pair<int,int>, vector<pair<int,int>> ,greater<pair<int,int>>> pq;
+        int n=nums.size();
+        vector<int> ans(nums.size());
+        for(int i=0;i<n;i++){
+            pq.push({nums[i],i});
+        }
         while(k--){
-            auto mi = min_element(ans.begin(),ans.end());
-            *mi=(m*(*mi));
+            pair<int,int> t=pq.top();
+            pq.pop();
+            t.first*=m;
+            pq.push(t);
+        }
+        while(!pq.empty()){
+            auto i=pq.top();
+            pq.pop();
+            ans[i.second]=i.first;       
         }
         return ans;
     }
